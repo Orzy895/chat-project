5cb0415e41c7df05e7ae123a38ffa7c242630261
Orzy895 <enrique.wen@utp.ac.pa>
2024-03-29 23:46:32 -0500

    changed socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
     const { authUser } = useAuthContext();
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://astrochat-a6xy.onrender.com", {
                 query: {
                     userID: authUser._id,
                 }
