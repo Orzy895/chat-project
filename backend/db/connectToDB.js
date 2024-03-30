d915f73859c5dcda98dcae04d94a585ba08e2821
Orzy895 <enrique.wen@utp.ac.pa>
2024-03-29 23:50:27 -0500

    db debugging

--- backend/db/connectToDB.js
+++ backend/db/connectToDB.js
@@ -5,6 +5,7 @@ const connectToDB = async() => {
         await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("Connected to DB");
     } catch (error) {
+        console.log(error);
         console.log("Error connecting to DB");
     }
 }
